0a0fa8d4c9584036e278f1d29d5b67ee30ed688a
RobotSitges <44520839+RobotSitges@users.noreply.github.com>
2021-01-11 19:26:00 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,15 @@ radio.onReceivedNumber(function (receivedNumber) {
     // Distància entre dos micro:bit
     basic.showNumber(Math.abs(radio.receivedPacket(RadioPacketProperty.SignalStrength) + 42))
 })
+let Tira = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+let rango = Tira.range(0, 3)
+wuKong.setLightMode(wuKong.LightMode.OFF)
 radio.setGroup(125)
+wuKong.lightIntensity(25)
+wuKong.setLightMode(wuKong.LightMode.BREATH)
+Tira.showColor(neopixel.colors(NeoPixelColors.Purple))
+Tira.showRainbow(1, 45)
 basic.forever(function () {
     basic.showIcon(IconNames.SmallDiamond)
+    basic.showNumber(Tira.length())
 })
